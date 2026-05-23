973552c333037622705a41d25449dd15ab847f7e
samuelcarlos2469 <samuelcarlos701estudos@gmail.com>
2026-05-23 15:42:32 -0400

    feat: initial vite config

---
+++ frontend/Holder/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import LoadingScreen from "./components/LoadingScreen";
+import './components/LoadingScreen.css';
+
+function App() {
+  return <LoadingScreen />;
+}
+
+export default App;
